ccccc95aa0006b1efd7b60cf6497dbf8587ef084
Simon <fsimone2002@gmail.com>
2022-04-30 11:32:35 +0200

    feat(code): add exercise files, will update later

---
+++ py/esercizio1.py
@@ -0,0 +1,17 @@
+def until_ten():
+    i = 0
+    while i < 10:
+        i += 1
+        print(i)
+
+
+until_ten()
+
+# parametrizzandola
+# def until_param(a):
+#     while a < 10:
+#         print(a)
+#         a += 1
+
+
+# until_param(5)
