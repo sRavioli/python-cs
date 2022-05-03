3009ece9342c5389e4986120a25bb1f64e484a02
Simon <fsimone2002@gmail.com>
2022-05-03 17:24:21 +0200

    feat: renamed to `02-3_L_estrai_indice_pari.py`

--- py/esercizio_3.py
+++
@@ -1,9 +0,0 @@
-# estraiamo tutti gli indici pari in una lista abritraria di 10 elementi in ordine inverso.
-# per farlo, usiamo la funzione range e lo slicing
-
-def get_index():
-    ll = list(range(10))
-    print(ll)
-    print(ll[-2::-2])
-
-get_index()
